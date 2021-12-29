0b5f75e130860101f6f3c397ca193073be6e9903
Rayhan1998 <miahrayhan01@gmail.com>
2021-12-29 16:08:22 +0600

    setup more endpoints for api calls

--- server.js
+++ server.js
@@ -27,19 +27,6 @@ const db = require("knex")({
   }
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Its working");
-});
-
-app.get("/savedrecipe/:email", (req, res) => {
-  db.select("*")
-    .from("usersavedrecipe")
-    .where("email", "=", req.params.email)
-    .then(data => {
-      res.send(data);
-    });
-});
-
 // app.get("/searchrecipe/:query", async (req, res) => {
 //   try {
 //     const response = await axios.get(
@@ -53,16 +40,8 @@ app.get("/savedrecipe/:email", (req, res) => {
 //   }
 // });
 
-// app.get("/searchrecipe/:query", (req, res) => {
-//   axios
-//     .get(
-//       `https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=1f1fe157&app_key=adf65b2d88f6b6e7d91211344c5b19a4`
-//     )
-//     .then(data => res.json(data.data.hits))
-//     .catch(err => res.send(err));
-// });
-
 app.get("/searchrecipe/:query", (req, res) => {
+  console.log(process.env.APIKEY);
   fetch(
     `https://api.edamam.com/api/recipes/v2?type=public&q=${req.params.query}&app_id=1f1fe157&app_key=${process.env.APIKEY}`
   )
@@ -71,6 +50,55 @@ app.get("/searchrecipe/:query", (req, res) => {
       res.status(200).send(data);
     })
     .catch(err => res.status(400).send(err));
+});
+
+app.get("/findrecipe/:foodtype", (req, res) => {
+  const { foodtype } = req.params;
+
+  if (foodtype !== "Dessert") {
+    fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${foodtype}&app_id=1f1fe157&app_key=${process.env.APIKEY}&imageSize=REGULAR&mealType=${foodtype}&Main%20course`
+    )
+      .then(response => response.json())
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => res.status(400).send(err));
+  } else if (foodtype === "Dessert") {
+    fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${foodtype}&app_id=1f1fe157&app_key=${process.env.APIKEY}&imageSize=REGULAR`
+    )
+      .then(response => response.json())
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => res.status(400).send(err));
+  }
+});
+
+app.get("/findcuisine/:foodtype", (req, res) => {
+  const { foodtype } = req.params;
+  fetch(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${foodtype}&app_id=1f1fe157&app_key=${process.env.APIKEY}&cuisineType=${foodtype}&imageSize=REGULAR&dishType=Main%20course`
+  )
+    .then(response => response.json())
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => res.status(400).send(err));
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("Its working");
+});
+
+app.get("/savedrecipe/:email", (req, res) => {
+  db.select("*")
+    .from("usersavedrecipe")
+    .where("email", "=", req.params.email)
+    .then(data => {
+      res.send(data);
+    });
 });
 
 app.post("/signin", (req, res) => {
